38c62dfaa96db8c7fcbe20b47df00d6032e394aa
Lucas Oliveira <luaks.skt8@gmail.com>
2023-03-30 21:39:02 -0300

    initial commit

---
+++ fish/.config/fish/aliases.fish
@@ -0,0 +1,34 @@
+
+##------------------------ LS COMMANDS
+alias l 'lsd'
+alias ls 'exa --icons'
+alias la 'lsd -la'
+alias ll 'lsd -l'
+
+
+##----------------------- USE EXA AS TREE
+alias t 'exa -T --icons'
+
+
+##----------------------- RM COMMANDS
+alias srmf 'sudo rm -rf'
+alias rmf 'rm -rf'
+
+
+##----------------------- PACMAN COMMANDS
+alias paclsi 'pacman -Qs'
+alias pacs 'sudo pacman -Sy'
+alias pacsy 'sudo pacman -Sy --no-confirm'
+alias pacsu 'sudo pacman -Syyu'
+alias pacsyu 'sudo pacman -Syy --no-confirm'
+alias pacss 'sudo pacman -Ssy'
+
+
+##----------------------- NANO COMMANDS
+alias nano 'nano'
+alias snano 'sudo nano'
+
+
+##----------------------- EMACS
+alias emc 'emacsclient -nc -a "emacs"'
+alias em 'emacsclient -t -a "emacs"'
---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    ## https://aur.archlinux.org/shell-color-scripts.git
+    colorscript -e crunchbang-mini
+end
+
+set -U fish_user_paths /usr/bin /usr/local/bin /usr/local/sbin /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
+
+set -U my_custom_paths $HOME/bin $HOME/.local/bin $HOME/.config/emacs/bin $HOME/Applications
+
+set -U fish_user_paths $fish_user_paths $my_custom_paths
+
+
+if [ -f $HOME/.config/fish/aliases.fish ]
+	source $HOME/.config/fish/aliases.fish
+end
+
+set fish_greeting ""
